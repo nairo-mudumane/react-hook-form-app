59502f40e5ed5938a1f078362120b0837885805f
nairo-mudumane <nairomudumane@outlook.com>
2022-05-24 10:38:38 +0200

    handling errors

--- src/App.tsx
+++ src/App.tsx
@@ -13,16 +13,22 @@ function App() {
         console.log(data);
     };
 
+    console.log(watch("email"));
+    console.log(errors);
     return (
         <div className="App">
             <form onSubmit={handleSubmit(onSubmit)}>
                 <input type="email" defaultValue="" {...register("email")} />
+                <br />
 
                 <input
                     type="password"
                     {...register("password", { required: true })}
                 />
+                <br />
+                {errors.password && <span>Password is required</span>}
 
+                <br />
                 <button>send</button>
             </form>
         </div>
